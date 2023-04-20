e0a0854d6ab587c0a82aa7133f084bee541857d5
fabrizio <fdellagiacoma@efs.at>
2023-04-20 09:03:02 +0200

    update

--- env.sh
+++ env.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 
 export VAR='first_variable_test'
+echo $VAR
+set VAR='first_test' # WIN
+echo $VAR
+VAR='test_auto_auto'
+echo $VAR
+sudo service docker start
 
+python app.py --terms 15
 
 echo 'env variables ready'
